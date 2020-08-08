dea1bc106773a023a4bb6a11364672d087ddfca7
Sara A <calisaraalhindi@gmail.com>
2020-08-08 20:36:03 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,6 +5,9 @@
 # }
 
 def happy_birthday(birthday_kids)
+  birthday_kids = { "Timmy" => 9,
+                    "Sarah" => 6,
+                    "Amanda" => 27}
   # add your code snippet here!
 end
 
