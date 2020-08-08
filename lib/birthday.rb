91bfb2c45174c16d707ab061c85291fbd667bf62
Sara A <calisaraalhindi@gmail.com>
2020-08-08 20:44:22 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,7 +6,16 @@
 
 def happy_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
-    if 
+    if age >= 12
   puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+else
+  
 end
+endwinner = ""
+passengers.each do |suite, name|
+  if suite == :suite_a && name.start_with?("A")
+    winner = name
+  end
 end
+ 
+winner
