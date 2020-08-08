c3dfeac7b0004032a1483bca982567505f1e1bf5
Sara A <calisaraalhindi@gmail.com>
2020-08-08 20:41:58 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,10 +5,7 @@
 # }
 
 def happy_birthday(birthday_kids)
-  birthday_kids = { "Timmy" => 9,
-                    "Sarah" => 6,
-                    "Amanda" => 27}
-  # add your code snippet here!
+  birthday_kids.each do |kids_name, age|
+  puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
 end
-
-
+end
