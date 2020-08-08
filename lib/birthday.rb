e74ea627adf7d6711b0c02db4d9d78a9ad67658a
Sara A <calisaraalhindi@gmail.com>
2020-08-08 20:48:20 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -8,8 +8,10 @@ def happy_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
     if age <= 12
       puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-else
-  puts "Happy Birthday #{kids_name}! You are now #{age} years old! You are too old for this."
+ You are too old for this."else
+  puts "Happy Birthday #{kids_name}! You are now #{age} years old!
 end
 end
 end
+
+def
